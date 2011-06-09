aaa623f4739783e80d35bbb70d51a8d572f8711c
mgencur@redhat.com <mgencur@redhat.com>
2011-06-09 17:16:18 +0200

    initial commit of carmart-single example

---
+++ carmart-single/readme.txt
@@ -0,0 +1,39 @@
+To run the example on JBoss AS 7 with EDG:
+
+1) add the following configuration to you $JBOSS_HOME/standalone/configuration/infinispan-configuration.xml:
+
+<namedCache name="carcache">
+      
+      <eviction wakeUpInterval="500"
+                maxEntries="4"
+                strategy="LIRS"/>
+
+      <loaders passivation="false"
+               shared="false"
+               preload="true">
+
+         <loader class="org.infinispan.loaders.file.FileCacheStore"
+                 fetchPersistentState="false"
+                 ignoreModifications="false"
+                 purgeOnStartup="false">
+
+            <properties>
+               <property name="location"
+                         value="${java.io.tmpdir}"
+                         />
+            </properties>
+
+            <async enabled="false" />
+         </loader>
+      </loaders>
+   </namedCache>
+   
+2) start the application server
+    
+    $JBOSS_HOME/bin/standalone.sh
+
+3) in the example's directory, run:
+
+    mvn clean install     (this will deploy the resulting war into AS)
+
+4) go to http://localhost:8080/carmart-single
---
+++ carmart-single/src/main/java/org/jboss/edg/examples/carmartsingle/session/CarManager.java
@@ -0,0 +1,100 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2009, Red Hat Middleware LLC, and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.edg.examples.carmartsingle.session;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.enterprise.inject.Model;
+import javax.inject.Inject;
+import org.infinispan.Cache;
+import org.jboss.edg.examples.carmartsingle.model.Car;
+
+/**
+ * 
+ * @author Martin Gencur
+ * 
+ */
+@Model
+public class CarManager
+{
+   public static final String CACHE_NAME = "carcache";
+   public static final String CAR_NUMBERS_KEY = "carnumbers";
+   
+   @Inject
+   private CacheContainerProvider provider;
+   
+   private Cache<String, Object> carCache;
+   
+   private String carId;
+   private Car car = new Car();
+
+   public CarManager() {
+   }
+   
+   public String addNewCar() {
+      carCache = provider.getCacheContainer().getCache(CACHE_NAME);
+      carCache.put(car.getNumberPlate(), car);
+      List<String> carNumbers = (ArrayList<String>) carCache.get(CAR_NUMBERS_KEY);
+      carNumbers.add(car.getNumberPlate());
+      carCache.replace(CAR_NUMBERS_KEY, carNumbers);
+      return "home";
+   }
+   
+   public String showCarDetails(String numberPlate) {
+      carCache = provider.getCacheContainer().getCache(CACHE_NAME);
+      this.car = (Car) carCache.get(numberPlate);
+      return "showdetails";
+   }
+   
+   public List<String> getCarList() {
+      //retrieve a cache
+      carCache = provider.getCacheContainer().getCache(CACHE_NAME);
+      //retrieve a list of number plates from the cache
+      List<String> carNumbers = (ArrayList<String>) carCache.get(CAR_NUMBERS_KEY);
+      return carNumbers;
+   }
+   
+   public String removeCar(String numberPlate) {
+       carCache = provider.getCacheContainer().getCache(CACHE_NAME);
+       carCache.remove(numberPlate);
+       List<String> carNumbers = (ArrayList<String>) carCache.get(CAR_NUMBERS_KEY);
+       carNumbers.remove(numberPlate);
+       carCache.replace(CAR_NUMBERS_KEY, carNumbers);
+       return null;
+   }
+   
+   public void setCarId(String carId) {
+      this.carId = carId;
+   }
+
+   public String getCarId() {
+      return carId;
+   }
+
+   public void setCar(Car car) {
+      this.car = car;
+   }
+
+   public Car getCar() {
+      return car;
+   }
+}
