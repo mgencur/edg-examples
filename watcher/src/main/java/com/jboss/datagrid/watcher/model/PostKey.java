cf6902feb05f428c67fedf327d26179e849949a9
Martin Gencur <mgencur@redhat.com>
2012-06-27 15:27:15 +0200

    Remove occurrences of the word tweet, rename the app to Watcher

---
+++ watcher/src/main/java/com/jboss/datagrid/watcher/model/PostKey.java
@@ -0,0 +1,80 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package com.jboss.datagrid.watcher.model;
+
+/**
+ * Identifies a post.
+ * 
+ * @author Martin Gencur
+ * 
+ */
+public class PostKey {
+
+   private String owner;
+   
+   private long timeOfPost;
+
+   public PostKey(String owner, long timeOfPost) {
+      this.owner = owner;
+      this.timeOfPost = timeOfPost;
+   }
+
+   public String getOwner() {
+      return owner;
+   }
+
+   public void setOwner(String username) {
+      this.owner = username;
+   }
+
+   public long getTimeOfPost() {
+      return timeOfPost;
+   }
+
+   public void setTimeOfPost(long timeOfPost) {
+      this.timeOfPost = timeOfPost;
+   }
+   
+   public int hashCode() {
+      final int prime = 31;
+      int result = 1;
+      result = prime * result + ((owner == null) ? 0 : owner.hashCode());
+      result = prime * result + (int) (timeOfPost ^ (timeOfPost >>> 32));
+      return result;
+   }
+
+   public boolean equals(Object obj) {
+      if (this == obj)
+         return true;
+      if (obj == null || getClass() != obj.getClass())
+         return false;
+      PostKey other = (PostKey) obj;
+      if (owner == null) {
+         if (other.owner != null)
+            return false;
+      } else if (!owner.equals(other.owner))
+         return false;
+      if (timeOfPost != other.timeOfPost)
+         return false;
+      return true;
+   }
+}
---
+++ watcher/src/main/java/com/jboss/datagrid/watcher/model/User.java
@@ -0,0 +1,138 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package com.jboss.datagrid.watcher.model;
+
+import java.awt.image.BufferedImage;
+import java.io.Serializable;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Describes a person. Also contains links to owned posts, watched/watching people.
+ * 
+ * @author Martin Gencur
+ * 
+ */
+public class User implements Serializable {
+
+   private static final long serialVersionUID = 188164481825309731L;
+
+   private String username; // unique identifier
+
+   private String name;
+   
+   private String password;
+   
+   private String whoami; // description of the person
+   
+   private BufferedImage avatar;
+   
+   private List<PostKey> posts;
+   
+   private List<String> watchers;
+   
+   private List<String> watching;
+
+   public User(String username, String name, String surname, String password, String whoami, BufferedImage avatar) {
+      this.username = username;
+      this.name = name;
+      this.password = password;
+      this.whoami = whoami;
+      this.posts = new LinkedList<PostKey>();
+      this.watchers = new LinkedList<String>();
+      this.watching = new LinkedList<String>();
+      this.avatar = avatar;
+   }
+
+   public void addFollower(String user) {
+      this.watchers.add(user);
+   }
+
+   public void removeFollower(String user) {
+      this.watchers.remove(user);
+   }
+
+   public void addFollowing(String user) {
+      this.watching.add(user);
+   }
+
+   public void removeFollowing(String user) {
+      this.watching.remove(user);
+   }
+
+   public void addPost(PostKey post) {
+      this.posts.add(post);
+   }
+
+   public String getUsername() {
+      return username;
+   }
+
+   public void setUsername(String username) {
+      this.username = username;
+   }
+
+   public String getName() {
+      return name;
+   }
+
+   public void setName(String name) {
+      this.name = name;
+   }
+
+   public String getPassword() {
+      return password;
+   }
+
+   public void setPassword(String password) {
+      this.password = password;
+   }
+
+   public String getWhoami() {
+      return whoami;
+   }
+
+   public void setWhoami(String whoami) {
+      this.whoami = whoami;
+   }
+
+   public List<PostKey> getPosts() {
+      return posts;
+   }
+
+   public List<String> getWatchers() {
+      return watchers;
+   }
+
+   public List<String> getWatching() {
+      return watching;
+   }
+
+   public BufferedImage getAvatar() {
+      return avatar;
+   }
+
+   public void setAvatar(BufferedImage avatar) {
+      this.avatar = avatar;
+   }
+
+}
